4e58213e91106212d1045cd8f4b5192c27584bf8
Shea Stevens <63370427+swstevens@users.noreply.github.com>
2024-07-05 14:23:15 -0700

    base metal objects

--- src/camera.h
+++ src/camera.h
@@ -4,6 +4,7 @@
 #include "rtweekend.h"
 
 #include "hittable.h"
+#include "material.h"
 
 class camera {
   public:
@@ -74,8 +75,11 @@ class camera {
             return color(0,0,0);
         hit_record rec;
         if (world.hit(r, interval(0.001, infinity), rec)) {
-            vec3 direction = rec.normal + random_unit_vector();
-            return 0.5 * ray_color(ray(rec.p, direction), depth-1, world);
+            ray scattered;
+            color attenuation;
+            if (rec.mat->scatter(r, rec, attenuation, scattered))
+                return attenuation * ray_color(scattered, depth-1, world);
+            return color(0,0,0);
         }
 
         vec3 unit_direction = unit_vector(r.direction());
