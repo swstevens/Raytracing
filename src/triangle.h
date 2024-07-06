b801892b7a397d2dae1388f32bd40212d141bbd3
Shea Stevens <63370427+swstevens@users.noreply.github.com>
2024-07-06 12:15:02 -0700

    working with triangles

---
+++ src/triangle.h
@@ -0,0 +1,59 @@
+#ifndef TRIANGLE_H
+#define TRIANGLE_H
+
+#include "hittable.h"
+#include "vec3.h"
+
+class triangle : public hittable {
+  public:
+        triangle(const point3& point_a, const point3& point_b, const point3& point_c, shared_ptr<material> mat)
+      : point_a(point_a), point_b(point_b), point_c(point_c), mat(mat) {}
+
+    bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
+      const float EPSILON = 0.0000001f;
+      vec3 edge1 = point_b - point_a;
+      vec3 edge2 = point_c - point_a;
+      vec3 h = cross(r.direction(), edge2);
+      float a = dot(edge1, h);
+
+      if (a > -EPSILON && a < EPSILON) {
+          return false; // Ray is parallel to the triangle
+      }
+
+      float f = 1.0f / a;
+      vec3 s = r.origin() - point_a;
+      float u = f * dot(s, h);
+
+      if (u < 0.0f || u > 1.0f) {
+          return false;
+      }
+
+      vec3 q = cross(s, edge1);
+      float v = f * dot(r.direction(), q);
+
+      if (v < 0.0f || u + v > 1.0f) {
+          return false;
+      }
+
+      const float t = f * dot(edge2, q);
+
+      if (t > EPSILON) {
+        // rec.t = root;
+        rec.p = r.at(rec.t);
+        vec3 outward_normal = unit_vector(cross(edge1, edge2));
+        rec.set_face_normal(r, -outward_normal);
+        rec.mat = mat;
+        return true;
+      }
+
+      return false;    
+    }
+
+  private:
+    point3 point_a;
+    point3 point_b;
+    point3 point_c;
+    shared_ptr<material> mat;
+};
+
+#endif
